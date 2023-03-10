2cf878ef85dac0164e80b7cad05f05ba6dbaa17b
DARSHAN-THE-CODER <reachdarshanv@gmail.com>
2023-03-10 17:06:02 +0530

    initial commit with little backend setup

---
+++ portfolio-backend/index.js
@@ -0,0 +1,32 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+app.use(bodyParser.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+// CORS
+app.use(cors());
+
+const defaultRouter = require('./routes/index')
+
+app.get("/", (req, res) => {
+    res.json({ message: "Server running" });
+});
+
+app.get('/api/v1',defaultRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+});
