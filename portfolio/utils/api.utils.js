bf824bfdbb57d77adeb99fae13129b8e06387230
DARSHAN-THE-CODER <reachdarshanv@gmail.com>
2023-03-18 01:52:08 +0530

    url add

--- portfolio/utils/api.utils.js
+++ portfolio/utils/api.utils.js
@@ -1,1 +1,2 @@
-export const APIURL = "http://localhost:8080/api/v1"
+// export const APIURL = "http://localhost:8080/api/v1"
+export const APIURL = "http://ec2-3-223-129-13.compute-1.amazonaws.com:8080/api/v1"
