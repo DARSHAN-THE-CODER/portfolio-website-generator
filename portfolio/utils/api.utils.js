62c1df1a17ff7f2ccb7f56da4950ca67605dd3f5
DARSHAN-THE-CODER <reachdarshanv@gmail.com>
2023-03-18 13:52:03 +0530

    cdfv

--- portfolio/utils/api.utils.js
+++ portfolio/utils/api.utils.js
@@ -1,3 +1,3 @@
 // export const APIURL = "http://localhost:8080/api/v1"
 // export const APIURL = "http://ec2-3-223-129-13.compute-1.amazonaws.com:8080/api/v1"
-export const APIURL = "https://portfolio.darshanv.website/api/v1/"
+export const APIURL = "https://portfolio.darshanv.website/api/v1"
